63c38017ce0c7c2e8668143e361815b47ca0da41
Norbert <kieler103@gmail.com>
2024-04-10 17:31:37 +0200

    mines!

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ UNIT_SIZE = 50
 RUNNING = True
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-win.fill((255, 255, 255))
+
 pygame.display.set_caption("Saper")
 MINE_IMG = pygame.image.load("mine.png")
 X, Y = 0, 0
 
-win.blit(MINE_IMG, (0, 0))
+
 def main():
-    generate_mines()
-    for i in range(int(WIDTH / UNIT_SIZE)):
-        for j in range(int(HEIGHT / UNIT_SIZE)):
-            pygame.draw.rect(win, (128, 128, 128), (X + (i * UNIT_SIZE), Y + (j * UNIT_SIZE), UNIT_SIZE, UNIT_SIZE), 1)
+    win.fill((255, 255, 255))
+    draw_board()
 
 
 def get_mouse_position():
@@ -27,20 +25,29 @@ def get_mouse_position():
     return mouse_x, mouse_y
 
 
+def draw_board():
+    for i in range(int(WIDTH / UNIT_SIZE)):
+        for j in range(int(HEIGHT / UNIT_SIZE)):
+            pygame.draw.rect(win, (128, 128, 128), (X + (i * UNIT_SIZE), Y + (j * UNIT_SIZE), UNIT_SIZE, UNIT_SIZE), 1)
+
+
 def generate_mines():
-    mine_list = []
-    for i in range(10):
-        x_random = random.randint(0, 10)
-        y_random = random.randint(0, 10)
-        mine_list.append((x_random, y_random))
+    all_coordinates = [(x, y) for x in range(11) for y in range(11)]
+    mine_list = random.sample(all_coordinates, 55)
     print(mine_list)
+    return mine_list
 
 
-def check_if_clicked_mine():
+def check_if_clicked_mine(mines):
     x, y = get_mouse_position()
-    pygame.draw.rect(win, (255, 0, 0), (x * UNIT_SIZE, y * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE), 4)
+    pygame.draw.rect(win, (105, 105, 105), (x * UNIT_SIZE, y * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE))
+    for i in range(len(mines)):
+        if mines[i][0] == x and mines[i][1] == y:
+            print("mine touched at", x, y)
+            win.blit(MINE_IMG, (x * UNIT_SIZE, y * UNIT_SIZE))
 
 
+MINES = generate_mines()
 main()
 
 
@@ -49,9 +56,7 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            check_if_clicked_mine()
-
-
+            check_if_clicked_mine(MINES)
     pygame.display.flip()
 
 
